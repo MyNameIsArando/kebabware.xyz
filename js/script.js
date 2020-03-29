26bda0430a833875dea7a16f241e8903ed790e53
mynameisarando <tylercrowhurst123@gmail.com>
2020-03-29 07:33:52 +0100

    bombaclatt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 document.getElementById('volume').volume = 0.04;
+
+$(function () {
+    count = 0;
+    wordsArray = ["Kebab Mecca North ", "#1 Source for Simcity 3000 hacks", "Stardew Valley Aimbotting", "No Ban No Vac No Infidel", "Bacon Detection", "Curbstomp Filthy White Men", "Mohammad (Peace be upon him)", "Halal Launcher", "Burqa Aimbot", "Wallhack no lag", "100% free no torrent", "no virus", "Sheep Fuck Not Included", "Malik Spinbot Toggle", "Cousin Fiddler", "AMD Anti-lag compatible", "Praise Allah", "Age of Consent? What?", "CS:GO BHop Scripts", "tips and tricks", "yallah yallah", "no motherfucker"];
+    setInterval(function () {
+      count++;
+      $("#word").fadeOut(400, function () {
+        $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+      });
+    }, 3000);
+  });
